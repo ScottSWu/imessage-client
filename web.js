54ad036a5262c3177da5d644e5f8069670a957ff
Scott Wu <scott.wu.hjx@gmail.com>
2017-12-05 01:08:51 -0500

    Add web service endpoints

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ const auth = require("express-basic-auth");
 const express = require("express");
 const fs = require("fs");
 const https = require("https");
-//const imessage = require("osa-imessage");
+const imessage = require("osa-imessage");
 const socket = require("socket.io");
 
 // Parse arguments
@@ -13,12 +13,31 @@ let pass = argv["pass"] || "pick a password";
 let key = argv["key"] || "privatekey.pem";
 let cert = argv["cert"] || "certificate.pem";
 
+// iMessage
+const messages = {};
+imessage.listen().on("message", data => {
+  const id = data.handle;
+  if (messages[id]) {
+    messages[id].push(data);
+  }
+  else {
+    messages[id] = [data];
+  }
+});
+
 // Setup the express server
 const app = express();
 const server = https.createServer({
   key: fs.readFileSync(key),
   cert: fs.readFileSync(cert),
 }, app);
+
+// Add authentication request header
+app.use(function(req, res, next) {
+  res.set("Access-Control-Allow-Headers", "Authorization");
+  res.set("Access-Control-Allow-Methods", "GET,POST");
+  next();
+});
 
 // Basic authentication
 let users = {};
@@ -38,14 +57,47 @@ app.get("/socket.io.js", function(req, res) {
 });
 
 // Dynamic routes
-app.get("/list", function(req, res) {
-  res.type("json");
-  res.send("{}");
+app.get("/list", function(req, res) { // List all chats
+  console.log("[/list]");
+  imessage.getRecentChats(20).then(chats => {
+    res.type("json");
+    res.send(JSON.stringify(chats.map(chat => {
+      return {
+        id: chat.recipientId,
+        service: chat.serviceName,
+      };
+    })));
+  });
 });
 
-app.post("/chat", function(req, res) {
+app.post("/chat", function(req, res) { // View a single chat
+  console.log("[/chat]");
+  const id = req.params["id"];
+  const chat = messages[id];
+  let start = 0;
+  if (req.params["since"] !== undefined) {
+    const since = new Date(req.params["since"]);
+    for (start = chat.length; start > 0; start--) {
+      if (since <= chat[start - 1].data) {
+        break;
+      }
+    }
+  }
+
+  const msgs = chat.slice(start);
   res.type("json");
-  res.send("{}");
+  res.send(JSON.stringify(msgs));
+});
+
+app.post("/send", function(req, res) { // Send a message
+  console.log("[/send]");
+  const id = req.params["id"];
+  const msg = req.params["msg"];
+  imessage.send(id, msg).then(v => {
+    res.sendStatus(200);
+  }).catch(e => {
+    res.sendStatus(500);
+  });
 });
 
 // Start the server
